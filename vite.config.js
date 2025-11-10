1acfe84346b2bd263a0b51e768ad9404cb3fe88c
Targaryen <leocj@live.com.co>
2025-11-09 22:33:26 -0500

    Post Entrega

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),tailwindcss()],
+  plugins: [react(),tailwindcss()],
+  base: "/Noticiero_Final_2025g1_Sanchez_Juan_David"
 })
