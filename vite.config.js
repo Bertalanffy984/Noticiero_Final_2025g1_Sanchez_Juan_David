4d7f47235c3250d4e2f4b78c2f7af004ab74693c
Bertalanffy984 <szjuanda@outlook.com>
2025-11-09 23:26:37 -0500

    final

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/Noticiero_Final_2025g1_Sanchez_Juan_David/"
 })
